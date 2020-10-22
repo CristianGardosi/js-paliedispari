c4a08c4308f035e381253a73d8fd6e613bfa918c
Cristian Gardosi <cristian.gardosi97@gmail.com>
2020-10-22 15:40:39 +0200

    PRIMA PARTE ESERCITAZIONE (palindroma) COMPLETA

--- js/script.js
+++ js/script.js
@@ -4,27 +4,30 @@
 // Creare una funzione per invertire i caratteri di una stringa.
 // Capire se la parola inserita è palindroma
 var parola = prompt('Inserire una parola');
-console.log('Parola inserita utente: ' + parola);
+console.log('Parola inserita: ' + parola);
 
+// Invoco la funzione per eseguire la mia inversione
 var invocazioneParolaInvertita = inversioneParola(parola);
+console.log('Parola invertita: ' + invocazioneParolaInvertita);
 
-console.log(invocazioneParolaInvertita);
+// Controllo se la parola inserita è palindroma
+if (parola === invocazioneParolaInvertita){
+    console.log('La parola inserita risulta essere PALINDROMA')
+} else {
+    console.log('La parola inserita non è PALINDROMA')
+}
 
+// Definisco la funzione (N.B. SEMPRE INFONDO AL MIO CODICE)
 function inversioneParola(parolaUtente) {
     var parolaInvertita = '';
     // La variabile i parte dalla fine della parolaUtente (il -1 serve perchè ricordiamo che il conteggio parte da 0 e non da 1) e continua il loop fino a quando non arriva al carattere 0 (il primo della parola inserita dall'utente) grazie al contatore 1--
-    for ( var i = parolaUtente.length - 1; i => 0; i-- ) {
+    for ( var i = parolaUtente.length - 1; i >= 0; i-- ) {
        parolaInvertita = parolaInvertita + parolaUtente[i];
       
     }
 
     return parolaInvertita;
 }
-
-
-
-
-
 
 
 // // PARI O DISPARI
